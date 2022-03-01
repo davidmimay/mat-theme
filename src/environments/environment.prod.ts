e368eaeab098b35acf8896cf56d2a00b4006b51f
david mimay <davidmimay@gmail.com>
2022-03-01 17:52:28 +0000

    🃏 cards grid updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBbefWnWSiTNNFj96m13CmgRiXIzqBFLr8",
+    authDomain: "mat-theme.firebaseapp.com",
+    projectId: "mat-theme",
+    storageBucket: "mat-theme.appspot.com",
+    messagingSenderId: "291365537352",
+    appId: "1:291365537352:web:3ae2e9ddbd2159efd2ba7d",
+    measurementId: "G-G5D33WB0Q5"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBbefWnWSiTNNFj96m13CmgRiXIzqBFLr8",
+    authDomain: "mat-theme.firebaseapp.com",
+    projectId: "mat-theme",
+    storageBucket: "mat-theme.appspot.com",
+    messagingSenderId: "291365537352",
+    appId: "1:291365537352:web:3ae2e9ddbd2159efd2ba7d",
+    measurementId: "G-G5D33WB0Q5"
+  }
 };
 
 /*
